5d226656e651afcd090eaf9e1b6a0887d7e43e9b
juan rodriguez <juanbrodriguez8@gmail.com>
2022-11-29 12:53:26 -0300

    cree vista de index y show para aircrafts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resource :aircrafts do
+  resources :aircrafts do
     resources :bookings
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
