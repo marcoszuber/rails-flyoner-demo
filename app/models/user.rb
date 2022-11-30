d8e89de78979a54f3990cc2f5f062e88c8e8c718
hellboy <matiascfh98@gmail.com>
2022-11-30 15:43:51 -0300

    Navbar and Devise updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :dni, presence: true, uniqueness: true, numericality: { only_integer: true }
   has_many :aircrafts, dependent: :destroy
   has_many :bookings, dependent: :destroy
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.active_storage.service = :cloudinary
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
