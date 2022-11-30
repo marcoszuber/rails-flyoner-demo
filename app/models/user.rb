5f83dc41f2a1e524d283ed26002aeedf5a984895
hellboy <matiascfh98@gmail.com>
2022-11-30 17:37:10 -0300

    Navbar updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :dni, presence: true, uniqueness: true, numericality: { only_integer: true }
   has_many :aircrafts, dependent: :destroy
   has_many :bookings, dependent: :destroy
