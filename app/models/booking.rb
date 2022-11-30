873913d1320467b6c283f0ced66e3ce1e74e7e49
Patricio Gallardo <patricio.gallardo@gmail.com>
2022-11-30 16:36:11 -0300

    Vista Booking - 90%

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :aircraft
+
+  validates :start_time, presence: true
+  validates :finish_time, presence: true
+
+  validates_comparison_of :finish_time, greater_than: :start_time, message: "Fecha de llegada debe ser mayor a la de salida"
 end
